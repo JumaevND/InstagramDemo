10f9e6c2464b6fbdaa5ab9fe50f9e81972162a69
NuriddinJumaev <nuriddin.jumaev98>
2020-12-02 15:17:59 +0500

    Initial Commit

---
+++ InstagramDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramDemo
+//
+//  Created by Nuriddin Jumaev on 12/2/20.
+//  Copyright © 2020 Nuriddin Jumaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
